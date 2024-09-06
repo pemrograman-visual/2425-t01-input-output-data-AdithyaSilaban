5856685f24bdb7eb6b0d8074b2e72f31a609d694
AdithyaSilaban <adithyasilaban@gmail.com>
2024-09-06 11:15:05 +0700

    Kami telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,1 +1,25 @@
-// NIM - Your Name
+// 12S24029 - Adithya Philip Jona Putra Silaban
+// 12S24048 - Olivia Tambunan
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, tT, penerbit, fBE, hP, mM, stok, rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tT = input.nextLine();
+        penerbit = input.nextLine();
+        fBE = input.nextLine();
+        hP = input.nextLine();
+        mM = input.nextLine();
+        stok = input.nextLine();
+        rating = input.nextLine();
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tT + "|" + penerbit + "|" + fBE + "|" + hP + "|" + mM + "|" + stok + "|" + rating);
+    }
+}
